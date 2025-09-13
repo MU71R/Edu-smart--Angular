515bf45760950982b0bd3aa58c04d6f6bde5409b
MU71R <mastafasamay351@gmail.com>
2025-09-13 23:36:02 +0300

    update dashboard

--- src/app/components/dashboard-course-details/dashboard-course-details.component.ts
+++ src/app/components/dashboard-course-details/dashboard-course-details.component.ts
@@ -2,37 +2,47 @@ import { Component } from '@angular/core';
 import { Cours } from 'src/app/models/cours';
 import { DashboardService } from 'src/app/services/dashboard.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-dashboard-course-details',
   templateUrl: './dashboard-course-details.component.html',
-  styleUrls: ['./dashboard-course-details.component.css' ]
+  styleUrls: ['./dashboard-course-details.component.css']
 })
 export class DashboardCourseDetailsComponent {
-  constructor(private dashboardService: DashboardService, private route: ActivatedRoute, private router: Router) { }
+  constructor(
+    private dashboardService: DashboardService, 
+    private route: ActivatedRoute, 
+    private router: Router,
+    private location: Location   
+  ) { }
+
   course: Cours | null = null;
   loading = true;
   error: string | null = null;
   id: string = '';
+
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id') || '';
     this.getCourseDetails();
   }
+
   getCourseDetails() {
     this.loading = true;
     this.dashboardService.getPendingCourses().subscribe({
-        next: (res: any) => {
-          this.course = res.find((c: any) => c._id === this.id) || null;
-          this.loading = false;
-        },
-        error: (err: any) => {
-          console.error(err);
-          this.loading = false;
-          this.error = err.message;
-        }
-      });
+      next: (res: any) => {
+        this.course = res.find((c: any) => c._id === this.id) || null;
+        this.loading = false;
+      },
+      error: (err: any) => {
+        console.error(err);
+        this.loading = false;
+        this.error = err.message;
+      }
+    });
   }
+
   approveInstructor(id: string) {
     this.dashboardService.updateCourseStatus(id, 'approve').subscribe({
       next: () => {
@@ -42,25 +52,24 @@ export class DashboardCourseDetailsComponent {
           timer: 2000,
           showConfirmButton: false,
         });
-        this.router.navigate(['/approve-courses']);
-        this.getCourseDetails();
+        this.location.back();   
       },
       error: (err) => {
         console.error(err);
       }
     });
   }
+
   rejectInstructor(id: string) {
     this.dashboardService.updateCourseStatusReject(id, 'reject').subscribe({
       next: () => {
-          Swal.fire({
-                  icon: 'success',
-                  title: 'تم رفض الدورة ❌',
-                  timer: 1500,
-                  showConfirmButton: false,
-                });
-                this.router.navigate(['/approve-courses']);
-        this.getCourseDetails();
+        Swal.fire({
+          icon: 'success',
+          title: 'تم رفض الدورة ❌',
+          timer: 1500,
+          showConfirmButton: false,
+        });
+        this.location.back();   
       },
       error: (err) => {
         console.error(err);
